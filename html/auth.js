1eeea470ed9cb83f451cbc9c97b9c38cc8322384
dogcraft <neko@neko.red>
2021-10-31 17:55:08 +0800

    upgrade

---
+++ html/auth.js
@@ -0,0 +1,42 @@
+
+function GetUserToken() {
+    doginfostr = localStorage.getItem('i');
+    if (doginfostr === null) {
+      console.log('未登录');
+      return false;
+    }
+    else {
+        ck = $.post("/api/i/",
+        {
+          i: doginfostr
+        },
+        function (data, status) {
+          if (data.r == 'OK') {
+              window.dog_info=data.c;
+              p1=document.getElementById("resddog");
+              p1.innerText=`您已经以${data.c.name}身份登录，再次登录可以切换用户或刷新用户信息。`;
+              p2=document.getElementById("usernamedog");
+              p2.innerText=data.c.name;
+              doginfo=JSON.stringify(data.c);
+              localStorage.setItem("doginfo",doginfo);
+          }
+          else {
+              console.log("登录错误。");
+          }
+        });
+    }
+}
+
+
+function dogin() {
+    iconurl=encodeURIComponent("https://www.dogcraft.top/avatar.jpg")
+    cburl=encodeURIComponent("http://127.0.0.1:5000/static/cb.html")
+    desp=encodeURIComponent("一个提问箱")
+    AuthUrl=`https://m.dogcraft.top/meow/?response_type=token&icon=${iconurl}&redirect_uri=${cburl}&name=Nyabox&desp=${desp}`
+    // console.log(AuthUrl);
+    window.location.href=AuthUrl;
+}
+
+window.onload=function () {
+    GetUserToken()
+}
---
+++ html/cb.js
@@ -0,0 +1,38 @@
+function GetDogToken() {
+        const url = decodeURI(location.hash); //获取url中"?"符后的字串
+        let theRequest = new Object();
+        if (url.indexOf("#") != -1) {
+           let str = url.substr(1);
+           strs = str.split("&");
+           for (let i = 0; i < strs.length; i++) {
+              theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+           }
+        }
+        return theRequest;
+}
+
+urlp=GetDogToken()
+
+console.log(urlp);
+
+if ((urlp.token_type != "key")) {
+    alert("错误调用！");
+} else {
+    ck = $.post("/api/login/",
+        {
+          t: urlp.access_token
+        },
+        function (data, status) {
+          if (data.r == 'OK') {
+              window.dog_info=data.r.c;
+              ii=data.token;
+              localStorage.setItem("i",ii);
+              window.location.href="auth.html";
+          }
+          else
+          {
+            alert("错误调用！");
+          }
+        })
+    }
+
