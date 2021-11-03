423bc9f06e9fbc041c849daacd94a03a9cbd0d90
dogcraft <neko@neko.red>
2021-11-04 07:50:38 +0800

    update

--- html/main.js
+++ html/main.js
@@ -1,1 +1,38 @@
-APIurl=""
+APIurl="";
+
+Nowpath=window.location.pathname;
+
+console.log(Nowpath);
+shu=Nowpath.split("/");
+
+if (shu[2]=="u") {
+    showUserPage();
+} else 
+
+{
+    
+}
+
+
+switch (shu[2]) {
+    case "u":
+    case "user":
+        showUserPage();
+        break;
+    case "home":
+        showHomePage();
+        break;
+    case "login":
+        showLoginPage();
+        break;
+    case "about":
+        showAboutPage();
+        break;
+    case "cb":
+        showCbPage();
+        break;
+    default:
+        showMainPage();
+        break;
+}
+
