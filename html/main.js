37b23dbecff54473fed4e2fe7deab985bcfb8450
dogcraft <neko@neko.red>
2022-01-02 10:42:18 +0800

    处理发布时间在"未来"的问题

--- html/main.js
+++ html/main.js
@@ -16,8 +16,8 @@ window.onload = function () {
 
 
 
-function CommitJSON(URL,data,callback) {
-    $.ajax({url: URL,type:"POST",contentType: "application/json",data: JSON.stringify(data),dataType: "json",success: callback});
+function CommitJSON(URL, data, callback) {
+    $.ajax({ url: URL, type: "POST", contentType: "application/json", data: JSON.stringify(data), dataType: "json", success: callback });
 }
 
 
@@ -32,14 +32,30 @@ function DogTimeDif(dateDiff) {
     var minutes = Math.floor(leave2 / (60 * 1000))//计算相差分钟数
     //计算相差秒数
     var leave3 = leave2 % (60 * 1000) //计算分
-    if (dayDiff >= 1) {
-        ResStr = `${dayDiff}天前 `;
-    } else if (hours >= 1) {
-        ResStr = `${hours}小时前 `;
-    }
-    else {
-        ResStr = `${minutes}分钟前 `
-    }
+    if (dateDiff < 0) {
+        dayDiff = -dayDiff;
+        hours = -hours;
+        minutes = -minutes;
+        if (dayDiff >= 1) {
+            ResStr = `${dayDiff}天后 `;
+        } else if (hours >= 1) {
+            ResStr = `${hours}小时后 `;
+        }
+        else {
+            ResStr = `${minutes}分钟后 `
+        }
+
+    } else {
+        if (dayDiff >= 1) {
+            ResStr = `${dayDiff}天前 `;
+        } else if (hours >= 1) {
+            ResStr = `${hours}小时前 `;
+        }
+        else {
+            ResStr = `${minutes}分钟前 `
+        }
+       
+    } 
     return ResStr;
 }
 
@@ -59,7 +75,7 @@ async function getUserInfo(uid) {
     if (window.doglist[uid]) {
         return window.doglist[uid];
     } else {
-        res = await CommitJSON(APIURL+"/api/getd/", {
+        res = await CommitJSON(APIURL + "/api/getd/", {
             t: uid
         });
         if (res.r == "OK" && res.c) {
@@ -77,13 +93,13 @@ function cklogin() {
     doginfo = localStorage.getItem("doginfo");
     if (doginfo) {
         // console.log(doginfo);
-        infodog=JSON.parse(doginfo);
-        const loginbt=document.getElementById("usernamedog");
-        loginbt.innerText=infodog.name;
-        loginbt.href="/home/";
+        infodog = JSON.parse(doginfo);
+        const loginbt = document.getElementById("usernamedog");
+        loginbt.innerText = infodog.name;
+        loginbt.href = "/home/";
     } else {
-        const loginbt=document.getElementById("usernamedog");
-        loginbt.href="/static/auth.html";
+        const loginbt = document.getElementById("usernamedog");
+        loginbt.href = "/static/auth.html";
     }
 }
 
