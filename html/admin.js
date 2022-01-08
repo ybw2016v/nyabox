790f6265a599c994cec34c638842f1f39ce0eb0d
dogcraft <neko@neko.red>
2022-01-08 15:10:02 +0800

    管理页面主要部分完成

--- html/admin.js
+++ html/admin.js
@@ -8,6 +8,40 @@ function AddAdminInfo(token) {
                 alert("错误，没有管理员权限");
                 window.location.href = "./";
             }
+            var INputDiv=`        <div class="modal fade" id="Upss">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- 模态框头部 -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">更新内容</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <!-- 模态框主体 -->
+                    <div class="modal-body">
+                    <label for="comment">内容:</label>
+                    <div id="quesdog">
+                    </div>
+                    <hr>
+                        <div class="form-group">
+                            <label for="comment">A:</label>
+                            <textarea class="form-control" placeholder="有什么想说的？" name="" id="AAnsdog" rows="4"></textarea>
+                        </div>
+                        <div>
+                        <input type="date" class="form-control" id="AAnsdate">
+                        <input type="time" class="form-control" id="AAnstime">
+                        </div>
+
+                    <div id="Loading"></div>
+                    </div>
+
+                    <!-- 模态框底部 -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" id="ansb" >更新</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+                    </div>
+                    </div>
+            </div>
+        </div>`;
             var AdminHomeHtml = `<div class="card">
             <div class="card-body" id="${data.c.uid}">
                 <img id="avatar-${data.c.uid}" src="${data.c.avatar}" class="dogavatar"><span class="dogname"><a href="/u/${data.c.uid}">管理员: ${data.c.name}</a></span>
@@ -23,6 +57,7 @@ function AddAdminInfo(token) {
             </div>
 
         </div>
+        ${INputDiv}
         
         <div id="hdlt"><div class="container mqus">
         <h5>问答管理</h5>
@@ -47,6 +82,7 @@ function AddAdminInfo(token) {
             Dql1.addEventListener("click", function () { AddAdminQ1(); });
             const Dql2 = document.getElementById("ql2");
             Dql2.addEventListener("click", function () { AddAdminQ2(); });
+            document.getElementById("ansb").addEventListener("click",PostUpdate);
 
         } else {
             alert("错误，该服务暂不可用");
@@ -120,7 +156,7 @@ function AdminAddQ(data) {
                 for (let j = 0; j < item.ans.length; j++) {
                     const ansdogitem = item.ans[j];
                     const adtmm = getDogDateTime(ansdogitem.time);
-                    AnsHtml += `<hr><div class="contain  ques" id="${ansdogitem.id}">
+                    AnsHtml += `<hr><div class="contain  ques" id="AD${ansdogitem.id}" qraw="${ansdogitem.c}" qtime="${ansdogitem.time}">
                     ${CMarkDown(ansdogitem.c)}
                 </div>
                 <div><button type="button" class="btn btn-primary" qid="${ansdogitem.id}" onclick="ADelQues(this);">
@@ -139,7 +175,7 @@ function AdminAddQ(data) {
             const Iadminhtml = `<div class="card">
             <div class="card-body" id="${item.id}">
                 <h6>Q:</h6>
-                <div class="contain  ques" id="QT${item.id}">
+                <div class="contain  ques" id="AD${item.id}" qtime="${item.time}">
                     ${CQues(item.c)}
                 </div>
                 <hr>
@@ -200,7 +236,9 @@ function AdminAddA(data) {
 
                 <hr>
                 <h6>A:</h6>
+                <div id="AD${item.id}" qraw="${item.c}" qtime="${item.time}">
                 ${CMarkDown(item.c)}
+                </div>
                 <hr>
             <div class="contain  ques" id="A-${item.id}">
             <button type="button" class="btn btn-primary" qid="${item.id}" onclick="ADelQues(this);">
@@ -315,6 +353,56 @@ function ADelQues(eldog) {
     else {
 
     }
+}
+
+function UpdateQA(dog) {
+    const qid = dog.getAttribute("qid");
+    document.getElementById("ansb").setAttribute("qid", qid);
+    huigdog=document.getElementById(`AD${qid}`);
+    if (huigdog.hasAttribute("qraw")) {
+        innerTextdog=huigdog.getAttribute("qraw");
+    } else {
+        innerTextdog=huigdog.innerText;
+    }
+    document.getElementById("AAnsdog").value=innerTextdog;
+    timedog=huigdog.getAttribute("qtime");
+    tyit=new Date(timedog);
+    inputdate=dateFormat("YYYY-mm-dd",tyit);
+    inputtime=dateFormat("HH:MM:SS",tyit);
+    console.log(inputtime);
+    console.log(inputdate);
+    document.getElementById("AAnsdate").value=inputdate;
+    document.getElementById("AAnstime").value=inputtime;
+    $("#Loading").text("");
+    $("#Upss").modal("show");
+    
+}
+
+function PostUpdate() {
+    console.log("IUPD");
+    qid=this.getAttribute("qid");
+    upc=document.getElementById("AAnsdog").value;
+    timestrdog=document.getElementById("AAnsdate").value+" "+document.getElementById("AAnstime").value;
+    ssu=new Date(timestrdog);
+    console.log(ssu.toISOString());
+    CommitJSON(APIURL + "/api/admin/upqa/", 
+    {
+        "i": token,
+        "t": qid,
+        "u":{"time":ssu.toISOString(),"c":upc},
+    },function (data, status) {
+        if (data.r == "OK") {
+            const SHtml = `<div class="alert alert-success alert-dismissible fade show">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>更新成功!</strong> 
+          </div>`;
+            $("#main").prepend(SHtml);
+            $("#Upss").click();
+        } else {
+            alert("更新失败，该服务暂不可用。");
+        }
+    });
+
 }
 
 function showAdminPage() {
